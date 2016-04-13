5417bb4beaca8e4282cacecffb7a41283cc14cc6
VasylKosowski <vasyl.kosowski@gmail.com>
2016-04-13 05:20:22 -0700

    - added angular and bootstrap; - implemented ui for register form;

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
  * Created by test on 4/12/16.
  */
 var express = require('express');
+//var path = require('path');
 var app = express();
 
+app.use('/modules', express.static(__dirname + '/node_modules/'));
+app.use('/styles', express.static(__dirname + '/styles/'));
 app.use(express.static(__dirname + "/public"));
 
 app.listen(8081);
